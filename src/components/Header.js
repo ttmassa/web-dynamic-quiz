ba5826475b06c8bb21c99dcb70a4659f0f10db08
Massa Timothée <timothee.massa.perso@gmail.com>
2023-11-24 12:40:13 +0100

    Relaod function added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,14 @@ import React from 'react'
 import logoImage from '../assets/logo-quiz.png'
 
 export default function Header() {
+    // Recharger la page quand on clique sur le logo
+    const refreshPage = () => {
+        window.location.reload()
+    }
+
     return (
        <header className='header--container'>
-            <img className='header--img' src={logoImage} alt='Quizz Break' />
+            <img className='header--img' src={logoImage} alt='Quizz Break' onClick={refreshPage}/>
             <h1 className='header--title'>Quizz Break</h1>
             <h3 className='header--descr'>Web dynamique - Final project</h3>
        </header>
